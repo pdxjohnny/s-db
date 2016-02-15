ae36eb5e6c35efae83b76a86b10b4d552e812120
John Andersen <johnandersenpdx@gmail.com>
2016-02-15 23:49:44 +0000

    Ensure index before each geospacial query (proablby bad)
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- db/near/near.go
+++ db/near/near.go
@@ -2,10 +2,13 @@ package near
 
 import (
 	"errors"
+	"fmt"
+	"log"
 
 	"github.com/pdxjohnny/s-db/db/shared"
 	"github.com/pdxjohnny/s-db/db/variables"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -17,8 +20,21 @@ func Near(collectionName string, longitude, latitude, radius float64) (*map[stri
 	}
 	collection := connection.DB(variables.DBName).C(collectionName)
 
+	setIndex := fmt.Sprintf("$%s:%s", "2dsphere", "location")
+
+	// Define the index
+	index := mgo.Index{
+		Key: []string{setIndex},
+	}
+
+	// Apply the index
+	err := collection.EnsureIndex(index)
+	if err != nil {
+		log.Println("ERROR creating 2dsphere index", err)
+	}
+
 	var found []map[string]interface{}
-	err := collection.Find(bson.M{
+	err = collection.Find(bson.M{
 		"location": bson.M{
 			"$nearSphere": bson.M{
 				"$geometry": bson.M{
